f9af68ce3f7b84969fe4432db926adb81b09326d
Andres <andres039039@hotmail.com>
2023-04-19 12:34:59 -0400

    add alloed origins

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const about_1 = __importDefault(require("./routers/about"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
-app.use("/api", about_1.default);
+const allowedOrigins = ["http://localhost:3000", "https://www.andresbeltran.dev"];
+app.use((0, cors_1.default)({
+    origin: allowedOrigins,
+}));
+app.use("/api", (0, cors_1.default)({
+    origin: allowedOrigins,
+}), about_1.default);
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
--- index.ts
+++ index.ts
@@ -1,15 +1,27 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
-import router from './routers/about'
-import cors from 'cors';
+import router from "./routers/about";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+const allowedOrigins = ["http://localhost:3000", "https://www.andresbeltran.dev"];
 
-app.use(cors());
-app.use("/api", router);
+app.use(
+  cors({
+    origin: allowedOrigins,
+  })
+);
+
+app.use(
+  "/api",
+  cors({
+    origin: allowedOrigins,
+  }),
+  router
+);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
